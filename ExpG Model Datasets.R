f76aef5fc4ea4f424daa9bbef6ef0d4fc0e7e523
fohanrahan <ferdiaoh@gmail.com>
2019-01-21 21:38:43 +0000

    Prevent strings converting to factors

--- ExpG Model Datasets.R
+++ ExpG Model Datasets.R
@@ -5,16 +5,12 @@ chances_csv_list <- list.files(path = 'C:/Users/Ferdia/Desktop/Football Data/Str
 mins_csv_list <- list.files(path = 'C:/Users/Ferdia/Desktop/Football Data/Strata Data/Minutes/', pattern = '.csv')
 
 # Import list of .csv files from directory
-chances_list <- lapply(paste0('C:/Users/Ferdia/Desktop/Football Data/Strata Data/Chances/',chances_csv_list), read.csv)
-mins_list <- lapply(paste0('C:/Users/Ferdia/Desktop/Football Data/Strata Data/Minutes/',mins_csv_list), read.csv)
+chances_list <- lapply(paste0('C:/Users/Ferdia/Desktop/Football Data/Strata Data/Chances/',chances_csv_list), function(x) read.csv(file = x, stringsAsFactors = FALSE))
+mins_list <- lapply(paste0('C:/Users/Ferdia/Desktop/Football Data/Strata Data/Minutes/',mins_csv_list), function(x) read.csv(file = x, stringsAsFactors = FALSE))
 
 # Bind files into single data frame
 chances <- do.call(rbind, chances_list)
 mins <- do.call(rbind, mins_list)
 
-# Delete row names column
-chances <- chances[, -c(1)]
-mins <- mins[, -c(1)]
-
-# Rename ID column to match chances data frame
+# Rename ID column in mins to match chances data frame
 colnames(mins)[5] <- "gsm_id"
