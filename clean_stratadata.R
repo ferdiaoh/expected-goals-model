ad2bae7cae3706484e1efc034bab3e712eba9caf
fohanrahan <ferdiaoh@gmail.com>
2019-01-30 22:47:33 +0000

    Add switch function

--- clean_stratadata.R
+++ clean_stratadata.R
@@ -1,64 +1,9 @@
 # Clean StrataBet data
 
+# Read in files
 source('~/GitHub/expected-goals-model/read_stratadata.R')
 
 library(tidyverse)
-
-
-# Works
-switch_test <- function(x) {
-  switch(x,
-         poorchance = 'Poor',
-         fairlygoodchance = 'Fairly Good',
-         goodchance = 'Good',
-         verygoodchance = 'Very Good',
-         greatchance = 'Great',
-         superbchance = 'Superb',
-         as.character(x)
-  )
-}
-
-chances$chanceRating <- sapply(chances$chanceRating, switch_test)
-
-# Doesn't work
-
-chances$chanceRating <- sapply(chances$chanceRating, switch,
-                               poorchance = 'Poor',
-                               fairlygoodchance = 'Fairly Good',
-                               goodchance = 'Good',
-                               verygoodchance = 'Very Good',
-                               greatchance = 'Great',
-                               superbchance = 'Superb',
-                               chances$chanceRating)
-
-chances <- chances %>%
-  mutate(chanceRating = case_when(chanceRating == 'poorchance' ~ 'Poor',
-                                  chanceRating == 'fairlygoodchance' ~ 'Fairly Good',
-                                  chanceRating == 'goodchance' ~ 'Good',
-                                  chanceRating == 'verygoodchance' ~ 'Very Good',
-                                  chanceRating == 'greatchance' ~ 'Great',
-                                  chanceRating == 'superbchance' ~ 'Suberb',
-                                  TRUE ~ as.character(chanceRating)))
-
-
-
-training$chanceRating <- ifelse(training$chanceRating=="fairlygoodchance", "Fairly Good",
-                                          ifelse(training$chanceRating=="verygoodchance", "Very Good",
-                                                 ifelse(training$chanceRating=="poorchance", "Poor",
-                                                        ifelse(training$chanceRating=="greatchance", "Great", training$chanceRating)))))
-
-training$type <- ifelse(training$type=="Direct free kick", "Direct Free-Kick", 
-                           ifelse(training$type=="Open play", "Open Play",
-                                  ifelse(training$type=="Direct corner", "Direct Corner", training$type)))
-
-  #exclude rows with dangerous moments (subjective measure of a chance ie. not a shot), penalties and direct corners in type column
-training <-training[!training$type=="Dangerous Moment" & !training$type=="Penalty" & !training$type=="Direct Corner",]
-
-training$primaryType <- ifelse(training$primaryType == "Shot/GK Save Rebound", "Shot (Opposition Rebound)",
-                                  ifelse(training$primaryType == "Woodwork Rebound", "Shot (Woodwork Rebound)",
-                                         ifelse(training$primaryType == "-", "No Assist", training$primaryType)))
-
-training$icon <- ifelse(training$icon == "goal", "yes","no")
 
 # Remove rows containing penalties, own goals or wining penalties
 chances <- chances %>%
@@ -66,38 +11,63 @@ chances <- chances %>%
            icon != 'penawarded' &
            icon != 'Penalty Missed' &
            icon != 'Penalty Awarded' &
+           type != 'Dangerous Moment' &
+           type != 'Penalty' &
+           type != 'Direct Corner' &
            chanceRating != 'Penalty' &
            chanceRating != '-')
 
-### TRANSFORMING X-AXIS
+# Create function to standardise variable names
+rename_variables <- function(x) {
+  switch(x,
+         poorchance = 'Poor',
+         fairlygoodchance = 'Fairly Good',
+         goodchance = 'Good',
+         verygoodchance = 'Very Good',
+         greatchance = 'Great',
+         superbchance = 'Superb',
+         `Direct free kick` = 'Direct Free-Kick',
+         `Open play` = 'Open Play',
+         `Direct corner` = 'Direct Corner',
+         `Shot/GK Save Rebound` = 'Shot (Opposition Rebound)',
+         `Woodwork Rebound` = 'Shot (Woodwork Rebound)',
+         `-` = 'No Assist',
+         as.character(x)
+  )
+}
 
-  #change negative co-ordinates to positive values by adding 136 to location_x
+# Apply rename_variables function to columns
+chances$chanceRating <- sapply(chances$chanceRating, rename_variables)
+chances$type <- sapply(chances$type, rename_variables)
+chances$primaryType <- sapply(chances$primaryType, rename_variables)
+
+# Create yes or no goal column
+training$icon <- ifelse(training$icon == "goal", "yes","no")
+
+# Transform x-axis
+# Co-ordinates = 0 to 272
 training$location_x <- c(as.numeric(training$location_x)) + 136
 
-### CALCULATING SHOT DISTANCE
-
-  #create function to calculate shot distance to centre of goal
-findDistance<-function(x, y){
+# Calculate shot distance
+find_distance <- function(x, y){
   x_off <- abs(136-x)
   y_off <- 0+y
-    #pythagoras' threorem
+    # Pythagoras' threorem
   distance <- sqrt((x_off*x_off)+(y_off*y_off))
   return(distance)
 }
 
-###CALCULATING SHOT ANGLE
-
-  #creates function to calculate shot angle to goal
-findAngle<-function(x, y){
-    #co-ordinates post to post
+# Calculate shot angle
+find_angle <- function(x, y){
+    # Co-ordinates post to post
   d1 = 30
-    #distance from ball to post 1
+    # Distance from ball to post 1
   d2 = findDistance(x-15, y)
-    #distance from ball to post 2
+    # Distance from ball to post 2
   d3 = findDistance(x+15, y)
-    #cosine rule
+    # Cosine rule
   angle = acos(((d2*d2)+(d3*d3)-(d1*d1))/(2*d2*d3))
-    #convert radians to degrees
+    # Convert radians to degrees
   angle <- (angle*180)/pi
   return(angle)
 }
@@ -105,8 +75,8 @@ findAngle<-function(x, y){
 ### CREATING VALUES FOR MODEL
 
   #divide shotDist by 4 to convert distance of co-ordinates to metres
-training$shotDist <- findDistance(as.numeric(training$location_x), as.numeric(training$location_y))/4
-training$shotAngle <- findAngle(as.numeric(training$location_x), as.numeric(training$location_y))
+training$shotDist <- find_distance(as.numeric(training$location_x), as.numeric(training$location_y))/4
+training$shotAngle <- find_angle(as.numeric(training$location_x), as.numeric(training$location_y))
 
   #change class of data for model
 training$icon <- as.factor(training$icon)
